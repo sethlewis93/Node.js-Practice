7dbcd27544eb249cc5381cbe8ae868360a6f447a
sethlewis93 <sbmail616@protonmail.com>
2020-10-29 11:25:35 -0400

    my changes

--- weather.js
+++ weather.js
@@ -4,35 +4,48 @@
 //Print
 
 const https = require("https");
-const querystring = require('querystring');
-const api = require('./api.json');
+const querystring = require("querystring");
+const api = require("./api.json");
+
+function printMessage(weather) {
+  const message = `${weather.id}'s weather report: ${weather.temp} with ${weather.description}`;
+  console.log(message);
+}
+
+function printError(error) {
+  console.error(error.message);
+}
 
 function get(query) {
   const parameters = {
-      APPID: api.key,
-      units: 'imperial'
+    APPID: api.key,
+    units: "imperial",
   };
-  
+
   const zipCode = parseInt(query);
-  if(!isNaN(zipCode)) {
-      parameters.zip = zipCode + ',us';
+  if (!isNaN(zipCode)) {
+    parameters.zip = zipCode + ",us";
   } else {
-      parameters.q = query + ',us';
+    parameters.q = query + ",us";
   }
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?${querystring.stringify(parameters)}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?${querystring.stringify(
+    parameters
+  )}`;
   console.log(url);
 
-  const request = https.get(url, response => {
-      let body = '';
-      response.on('data', chunk => {
-          body += chunk;
-      })
-      response.on('end', () => {
-          console.log(body);
-      })
-  })
+  const request = https.get(url, (response) => {
+    let body = "";
+    response.on("data", (chunk) => {
+      body += chunk;
+    });
+    response.on("end", () => {
+      const weather = JSON.parse(body);
+      printMessage(weather);
+    });
+  });
 
+  request.on("error", printError);
 }
 
 module.exports.get = get;
