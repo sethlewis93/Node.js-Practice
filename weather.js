64d1ef6e7748f33c9ae6a68a9c863c875a2bc66d
sethlewis93 <sbmail616@protonmail.com>
2020-10-29 22:22:52 -0400

    various changes

--- weather.js
+++ weather.js
@@ -1,51 +1,69 @@
-//Connect
-//Read
-//Parse
-//Print
-
+const http = require("http");
 const https = require("https");
 const querystring = require("querystring");
 const api = require("./api.json");
 
-function printMessage(weather) {
-  const message = `${weather.id}'s weather report: ${weather.temp} with ${weather.description}`;
+// Print out temp details
+function printWeather(weather) {
+  const message = `Current temperature in ${weather.name} is ${weather.main.temp}F`;
   console.log(message);
 }
 
+// Print out error message
 function printError(error) {
   console.error(error.message);
 }
 
 function get(query) {
-  const parameters = {
-    APPID: api.key,
-    units: "imperial",
-  };
+  try {
+    const parameters = {
+      APPID: api.key,
+      units: "imperial",
+    };
 
-  const zipCode = parseInt(query);
-  if (!isNaN(zipCode)) {
-    parameters.zip = zipCode + ",us";
-  } else {
-    parameters.q = query + ",us";
+    const zipCode = parseInt(query);
+    if (!isNaN(zipCode)) {
+      parameters.zip = zipCode + ",us";
+    } else {
+      parameters.q = query + ",us";
+    }
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?${querystring.stringify(
+      parameters
+    )}`;
+    console.log(url);
+
+    const request = https.get(url, (response) => {
+      if (response.statusCode === 200) {
+        let body = "";
+        // Read the data
+        response.on("data", (chunk) => {
+          body += chunk;
+        });
+        response.on("end", () => {
+          try {
+            //Parse data
+            const weather = JSON.parse(body);
+            //Print the data
+            printWeather(weather);
+          } catch (error) {
+            //Parser error
+            printError(error);
+          }
+        });
+      } else {
+        // Status error code
+        const statusErrorCode = new Error(
+          `There was an error getting the message for "${query}". (${
+            http.STATUS_CODES[response.statusCode]
+          })`
+        );
+        printError(statusErrorCode);
+      }
+    });
+  } catch (error) {
+    printError(error);
   }
-
-  const url = `https://api.openweathermap.org/data/2.5/weather?${querystring.stringify(
-    parameters
-  )}`;
-  console.log(url);
-
-  const request = https.get(url, (response) => {
-    let body = "";
-    response.on("data", (chunk) => {
-      body += chunk;
-    });
-    response.on("end", () => {
-      const weather = JSON.parse(body);
-      printMessage(weather);
-    });
-  });
-
-  request.on("error", printError);
 }
 
 module.exports.get = get;
